25c6f81806d9104c736f0e1e4b64a357e7c766ea
Roberto <petrillo.r0@gmail.com>
2014-10-08 18:49:49 +0200

    HelloNode generato sul mio PC e pushato tramite GIT.

---
+++ HelloNode/Hello.js
@@ -0,0 +1,17 @@
+var http = require('http');
+
+function get(req, res) {
+	
+	var body = "Questa è una prova scritta con eclipse!<br>";
+	body += new Date();
+	var len = body.length+1;
+	
+	res.writeHead(200, {
+		'Content-Length': len,
+		'Content-Type': 'text/html'
+	});
+	res.end(body);
+}
+
+var s = http.createServer(get);
+s.listen(8080);
